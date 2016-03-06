ca8f2e5356f2bdb5145c4a362889ebe8c8a50836
lbernardjonesjr <l.bernard.jones.jr@gmail.com>
2016-03-06 16:20:57 -0500

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -1,6 +1,6 @@
 dataFile <- readRDS("Source_Classification_Code.rds")
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+data1 <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data1[data1$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 activePower <- as.numeric(subSetData$Global_active_power)
 png("plot2.png", width=480, height=480)
