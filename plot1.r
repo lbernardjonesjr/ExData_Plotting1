55e8393d8a94140e5ba1043071b3c9e37de95014
lbernardjonesjr <l.bernard.jones.jr@gmail.com>
2016-03-06 16:18:37 -0500

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,4 +1,4 @@
-dataFile <- "Source_Classification_Code.rds"
+dataFile <- readRDS("Source_Classification_Code.rds")
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 activePower <- as.numeric(subSetData$Global_active_power)
